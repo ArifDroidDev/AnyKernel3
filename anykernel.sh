e1b882f44f1e0c4fd20ed249ebe7334a3e267016
ArifDroidDev <91365852+ArifDroidDev@users.noreply.github.com>
2021-10-06 06:56:18 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExtraJossv3 by Arif Munandar @Munandar17
+kernel.string=ExtraJoss_v4 by Arif Munandar @Munandar17
 do.devicecheck=1
 do.modules=0
 do.systemless=1
